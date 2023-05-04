afbce1f1223ae26f2e5a1af9660f8a595acbd8f8
EttoreToma <ettore.toma@gmail.com>
2023-05-04 21:33:25 +0200

    Initial Commit

---
+++ Popcorn/PopcornApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopcornApp.swift
+//  Popcorn
+//
+//  Created by Ettore Toma on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PopcornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
